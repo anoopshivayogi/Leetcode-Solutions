28a64b5efb9f7ce674b403f5bf132b0b2b03bf0a
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-10-06 03:36:54 -0400

    Time: 48 ms (59.44%), Space: 18 MB (76.86%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -15,25 +15,48 @@ class Solution:
         # Time - O(n)
         # Space - O(n)
 
-        if not root:
-            return root
+        # if not root:
+        #     return root
 
-        q = collections.deque()
-        q.append(root)
+        # q = collections.deque()
+        # q.append(root)
 
-        while q:
+        # while q:
             
-            q_len = len(q)
-            for i in range(q_len):
-                cur_node = q.popleft()
+        #     q_len = len(q)
+        #     for i in range(q_len):
+        #         cur_node = q.popleft()
 
-                if i < q_len - 1:  # NOTE: one less than the last
-                    cur_node.next = q[0]
+        #         if i < q_len - 1:  # NOTE: one less than the last
+        #             cur_node.next = q[0]
                 
-                if cur_node.left:
-                    q.append(cur_node.left)
+        #         if cur_node.left:
+        #             q.append(cur_node.left)
 
-                if cur_node.right:
-                    q.append(cur_node.right)
+        #         if cur_node.right:
+        #             q.append(cur_node.right)
+
+        # return root
+
+
+        # NOTE: Followup will come that you have to do this with O(1) space excluding recursion stack
+
+        # Solution 2 - 
+        # Time -
+        # Space -
+
+        cur, nxt = root, root.left if root else None
+
+        while cur and nxt:
+            cur.left.next = cur.right
+
+            if cur.next: # If cur.next is not null only then you have to do this 
+                cur.right.next = cur.next.left
+
+            cur = cur.next
+
+            if not cur:   # Go to the next level if there is nothing on the right
+                cur = nxt
+                nxt = cur.left
 
         return root
