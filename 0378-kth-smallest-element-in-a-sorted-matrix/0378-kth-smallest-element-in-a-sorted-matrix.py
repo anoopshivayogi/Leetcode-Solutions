1880ab0d16a00450f197c0a21aa80cf3843c058c
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-10-20 11:45:50 -0700

    Time: 0 ms (100%), Space: 20.3 MB (98.85%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -17,27 +17,63 @@ class Solution:
         # 2   [12, 13, 15]]
 
         
-        n = len(matrix)
-        min_heap = []
+        # n = len(matrix)
+        # min_heap = []
 
-        for r in range(min(n, k)):
-            min_heap.append((matrix[r][0], r, 0))
+        # for r in range(min(n, k)):
+        #     min_heap.append((matrix[r][0], r, 0))
 
-        heapq.heapify(min_heap)
+        # heapq.heapify(min_heap)
 
-        while k > 1:
+        # while k > 1:
 
-            cur_min, row, col = heapq.heappop(min_heap)
+        #     cur_min, row, col = heapq.heappop(min_heap)
 
-            if col + 1 < n:
-                heapq.heappush(min_heap, (matrix[row][col + 1], row, col + 1))
+        #     if col + 1 < n:
+        #         heapq.heappush(min_heap, (matrix[row][col + 1], row, col + 1))
 
-            k -= 1
+        #     k -= 1
 
-        return min_heap[0][0]
+        # return min_heap[0][0]
 
 
         # Solution 2 - Using binary search
         # Time - 
         # Space - 
 
+        def get_less_than_equal(mid):
+            count = 0
+
+            n = len(matrix)
+            smaller, larger = matrix[0][0], matrix[n-1][n-1]
+            row, col = n-1, 0
+
+            while row >= 0 and col < n:
+
+                if matrix[row][col] <= mid:
+                    smaller = max(smaller, matrix[row][col])
+                    col += 1
+                    count += row + 1
+                else:
+                    larger = min(larger, matrix[row][col])
+                    row -= 1
+
+            return count, smaller, larger
+
+
+        n = len(matrix)
+        low, high = matrix[0][0], matrix[n-1][n-1]
+
+        while low < high:
+            
+            mid = (low + high) // 2
+            count, smaller, larger = get_less_than_equal(mid)
+
+            if count == k:
+                return smaller
+            if count < k:
+                low = larger
+            else:
+                high = smaller
+
+        return low
