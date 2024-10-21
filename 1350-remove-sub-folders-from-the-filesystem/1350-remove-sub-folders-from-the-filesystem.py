fcd4b56dc712ef37537a21a8af999d6079d1a400
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-10-21 15:44:56 -0700

    Time: 51 ms (96.54%), Space: 29.6 MB (98.16%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.py
@@ -2,14 +2,14 @@ class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
         
         # Solution 1 - Using sorting and seen 
-        # Time - 
-        # Space - 
+        # Time - O(n)
+        # Space - O(n) or O(1) if you do not consider the resultant array.
 
         folder.sort()
 
         res = [folder[0]]
 
-        for f in folder[1: ]:
+        for f in folder[1:]:
             exist = False
             for idx, ch in enumerate(f):
                 if ch == "/":
