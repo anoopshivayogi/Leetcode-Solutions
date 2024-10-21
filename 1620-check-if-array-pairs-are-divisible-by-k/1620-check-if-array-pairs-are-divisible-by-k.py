f5a48eec2b948c93b38a834904fdca8a1f95b532
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-10-21 14:48:29 -0700

    Time: 103 ms (98.74%), Space: 30.6 MB (16.19%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.py
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.py
@@ -20,20 +20,52 @@ class Solution:
         # You need to mod it again after converting from negative to positive.
 
 
-        remainder = collections.defaultdict(int)
+        # remainder = collections.defaultdict(int)
 
-        for num in arr:
-            remainder[(num % k + k) % k] += 1
+        # for num in arr:
+        #     remainder[(num % k + k) % k] += 1
 
-        for num in arr:
+        # for num in arr:
             
-            cur_rem = (num % k + k) % k
+        #     cur_rem = (num % k + k) % k
 
-            if cur_rem == 0:
-                if remainder[cur_rem] % 2:  # If the reminder 0 has odd occurences then it is False
-                    return False
+        #     if cur_rem == 0:
+        #         if remainder[cur_rem] % 2:  # If the reminder 0 has odd occurences then it is False
+        #             return False
 
-            elif remainder[k - cur_rem] != remainder[cur_rem]:
+        #     elif remainder[k - cur_rem] != remainder[cur_rem]:
+        #         return False
+
+        # return True
+
+
+        # Solution 2 - Sorting and Two pointers 
+        # Time - O(nlogn)
+        # Space - O(n) for sorting ;In Python, the sort method sorts a list using the Timsort algorithm which is a combination of Merge Sort and Insertion Sort and has O(n) additional space.
+        # Space can be constant if we don't account for the sorting space.
+
+
+        arr = sorted(arr, key=lambda num: (num % k + k) % k)
+
+        left, right = 0, len(arr) - 1
+
+        while left < right:     # Skip all the numbers with zero remainders
+
+            if arr[left] % k != 0:
+                break
+
+            if arr[left + 1] % k != 0:
                 return False
+
+            left += 2
+
+        
+        while left < right:
+
+            if (arr[left] + arr[right]) % k != 0:
+                return False
+
+            left += 1
+            right -= 1
 
         return True
