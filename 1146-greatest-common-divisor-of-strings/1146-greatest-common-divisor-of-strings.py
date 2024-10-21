2735401c6b7a2112b0f1193782e4c30c967e4a86
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-10-21 08:21:28 -0700

    Time: 0 ms (100%), Space: 16.6 MB (24.17%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -1,18 +1,20 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
 
+        # Solution 1 - 
 
-        # Solution 2 - 
-        # Time - 
-        # Space - 
 
-        def gcd(a, b):
+
+        # Solution 2 - Using Euclidean formula for calculating gcd
+        # Time - O(log(min(str1, str2)))
+        # Space - O(1)
+
+        def gcd(a, b):  # O(log(min(a, b)))
 
             while b > 0:
                 a, b = b, a % b
 
             return a
-
         
         if str1 + str2 != str2 + str1:
             return ""
