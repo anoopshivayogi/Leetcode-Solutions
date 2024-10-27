6eefa888b4a4ea6cf0fa5da6fe5dc3fb178d3be8
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-10-26 20:06:00 -0700

    Time: 0 ms (100%), Space: 16.5 MB (35.34%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.py
+++ 0812-rotate-string/0812-rotate-string.py
@@ -6,11 +6,18 @@ class Solution:
         # Time - O(N^2)
         # Space - O(1)
 
-        if len(s) != len(goal):
-            return False
+        # if len(s) != len(goal):
+        #     return False
 
-        for i in range(len(s)):
-            if s[i:] + s[:i] == goal:
-                return True
+        # for i in range(len(s)):
+        #     if s[i:] + s[:i] == goal:
+        #         return True
 
-        return False
+        # return False
+
+
+        # Solution 1 - 
+        # Time - 
+        # Space - 
+
+        return len(s) == len(goal) and goal in s + s
