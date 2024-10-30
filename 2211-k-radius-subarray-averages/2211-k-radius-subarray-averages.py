dda71de388e8566e4fa9c88f170c02d07c915c5b
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-10-29 22:10:11 -0700

    Time: 99 ms (56.1%), Space: 30.9 MB (93.14%) - LeetHub

--- 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.py
+++ 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.py
@@ -4,28 +4,28 @@ class Solution:
         # Time - O(n)
         # Space - O(n)
 
-        window_size = (k * 2) + 1
-        n = len(nums)
-        prefix = [0] * (n + 1)
-        res = [-1] * n
+        # window_size = (k * 2) + 1
+        # n = len(nums)
+        # prefix = [0] * (n + 1)
+        # res = [-1] * n
 
-        for i in range(n):
-            prefix[i + 1] = prefix[i] + nums[i]
+        # for i in range(n):
+        #     prefix[i + 1] = prefix[i] + nums[i]
 
-        for i in range(k, n - k):
-            left_bound, right_bound = i - k, i + k
-            sub_array = prefix[right_bound + 1] - prefix[left_bound]
-            average = sub_array // window_size
-            res[i] = average
+        # for i in range(k, n - k):
+        #     left_bound, right_bound = i - k, i + k
+        #     sub_array = prefix[right_bound + 1] - prefix[left_bound]
+        #     average = sub_array // window_size
+        #     res[i] = average
 
-        return res
+        # return res
 
         
         # Solution 1 - Using sliding window
         # Time - O(2n)
         # Space - O(1)
 
-        # if k == 0:
+        # if k == 0: # NOTE: Edge condition in the interview 
         #     return nums
 
         # win_count = 0
@@ -52,21 +52,23 @@ class Solution:
         # Time - O(n)
         # Space - O(1)
 
+        if k == 0: # NOTE: Edge condition in the interview 
+            return nums
 
-        # win_count = 0
-        # r = len(nums) - 1
-        # res = [-1] * len(nums)
-        # window_size = (k*2) + 1
+        win_count = 0
+        r = len(nums) - 1
+        res = [-1] * len(nums)
+        window_size = (k*2) + 1
 
-        # if window_size > len(nums):  # NOTE: Edge condition for the interview 
-        #     return res
+        if window_size > len(nums):  # NOTE: Edge condition for the interview 
+            return res
 
-        # for r in range(len(nums)):
-        #     win_count += nums[r]
+        for r in range(len(nums)):
+            win_count += nums[r]
 
-        #     if r >= (k * 2):
-        #         l = r - (k * 2)
-        #         res[l+k] = int(win_count / ((k*2) + 1))
-        #         win_count -= nums[l]
+            if r >= (k * 2):
+                l = r - (k * 2)
+                res[l+k] = int(win_count / ((k*2) + 1))
+                win_count -= nums[l]
 
-        # return res
+        return res
