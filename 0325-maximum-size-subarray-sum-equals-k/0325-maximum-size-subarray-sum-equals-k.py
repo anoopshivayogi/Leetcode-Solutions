760f42e5456d2e51adc56b609ff51d2e8f96822f
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-11-01 08:49:09 -0700

    Time: 147 ms (57.51%), Space: 62.2 MB (21.95%) - LeetHub

---
+++ 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxSubArrayLen(self, nums: List[int], k: int) -> int:
+        
+
+        # [1, -1, 5, -2, 3], k = 3
+        #  0.  1. 2.  3. 4
+        #             i
+        # total = 3
+        # seen = {0: -1, 1: 0, 5: 2}
+        # total - k = 0
+
+
+        # Solution 1 - Prefix sum with hashmap
+        # Time - 
+        # Space - 
+
+        seen = {0: -1}
+        cur_sum = 0
+        res = 0
+
+        for idx, num in enumerate(nums):
+            cur_sum += num
+
+            if (cur_sum - k) in seen:
+                res = max(idx - seen[(cur_sum - k)], res)
+            
+            if cur_sum not in seen:
+                seen[cur_sum] = idx
+
+        return res
