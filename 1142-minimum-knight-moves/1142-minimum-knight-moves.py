faad3ea2abd86985aaf29a5ff4af3bd07ff75870
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-11-01 22:18:40 -0700

    Time: 15 ms (94.72%), Space: 18 MB (85.19%) - LeetHub

--- 1142-minimum-knight-moves/1142-minimum-knight-moves.py
+++ 1142-minimum-knight-moves/1142-minimum-knight-moves.py
@@ -9,32 +9,46 @@ class Solution:
         # https://medium.com/nerd-for-tech/minimum-knight-moves-daily-challenge-may-4499e5217df5
 
 
-        directions = [(1, 2), (2, 1), (-1, -2), (-2, -1), (2, -1), (1, -2), (-2, 1), (-1, 2)]
+        # directions = [(1, 2), (2, 1), (-1, -2), (-2, -1), (2, -1), (1, -2), (-2, 1), (-1, 2)]
 
-        x = abs(x) # NOTE: Shifting to the first quadrant 
-        y = abs(y) # NOTE: Shifting to the first quadrant
+        # x = abs(x) # NOTE: Shifting to the first quadrant 
+        # y = abs(y) # NOTE: Shifting to the first quadrant
 
-        q = collections.deque()
-        visit = set()
-        q.append((0, 0, 0))  # x, y, moves
-        visit.add((0, 0))
+        # q = collections.deque()
+        # visit = set()
+        # q.append((0, 0, 0))  # x, y, moves
+        # visit.add((0, 0))
 
-        while q:
-            cur_x, cur_y, cur_mov = q.popleft()
+        # while q:
+        #     cur_x, cur_y, cur_mov = q.popleft()
 
-            if cur_x == x and cur_y == y:
-                return cur_mov
+        #     if cur_x == x and cur_y == y:
+        #         return cur_mov
 
-            for inc_x, inc_y in directions:
-                new_r, new_c = cur_x + inc_x, cur_y + inc_y
+        #     for inc_x, inc_y in directions:
+        #         new_r, new_c = cur_x + inc_x, cur_y + inc_y
 
-                if (new_r, new_c) not in visit and -2 <= new_r < x + 2 and -2 <= new_c < y + 2:  # NOTE: Restricting the boundary is very important 
-                    q.append((new_r, new_c, cur_mov + 1))
-                    visit.add((new_r, new_c))
+        #         if (new_r, new_c) not in visit and -2 <= new_r < x + 2 and -2 <= new_c < y + 2:  # NOTE: Restricting the boundary is very important 
+        #             q.append((new_r, new_c, cur_mov + 1))
+        #             visit.add((new_r, new_c))
 
-        return -1  # NOTE: Not Necessary
+        # return -1  # NOTE: Not Necessary
 
 
         # Solution 2 - Using DFS
         # Time - 
         # Space - 
+
+        seen = {(0, 0): 0, (1, 1): 2, (0, 2): 2, (2, 0): 2, (0, 1): 3, (1, 0): 3}
+
+        from functools import cache
+
+        @cache
+        def dfs(x, y):
+
+            if (x, y) in seen:
+                return seen[(x, y)]
+
+            return 1 + min(dfs(abs(x-2), abs(y-1)), dfs(abs(x-1), abs(y-2)))
+
+        return dfs(abs(x), abs(y))
