b9fef5c8ff8f025596222388cc5d8768e71a81a7
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-11-02 16:32:49 -0700

    Time: 27 ms (45.27%), Space: 17.5 MB (41.54%) - LeetHub

--- 0536-construct-binary-tree-from-string/0536-construct-binary-tree-from-string.py
+++ 0536-construct-binary-tree-from-string/0536-construct-binary-tree-from-string.py
@@ -12,45 +12,86 @@ class Solution:
         # Time - O(N)
         # Space - O(N)
         
-        if not s:  # NOTE: Clarify what to return in this case with the interviewer
-            return None
+        # if not s:  # NOTE: Clarify what to return in this case with the interviewer
+        #     return None
 
-        st = []
-        i = 0
+        # st = []
+        # i = 0
 
-        while i < len(s):
+        # while i < len(s):
             
-            if s[i] == "-" or s[i].isdigit():
-                val = 0
-                neg = False
+        #     if s[i] == "-" or s[i].isdigit():
+        #         val = 0
+        #         neg = False
 
-                if s[i] == "-":
-                    i += 1
-                    neg = True
+        #         if s[i] == "-":
+        #             i += 1
+        #             neg = True
 
-                while i < len(s) and s[i].isdigit():
-                    val = val * 10 + int(s[i])
-                    i += 1
+        #         while i < len(s) and s[i].isdigit():
+        #             val = val * 10 + int(s[i])
+        #             i += 1
 
-                i -= 1
+        #         i -= 1
 
-                if neg:
-                    cur_node = TreeNode(-val)
-                else:
-                    cur_node = TreeNode(val)
+        #         if neg:
+        #             cur_node = TreeNode(-val)
+        #         else:
+        #             cur_node = TreeNode(val)
                 
-                st.append(cur_node)
+        #         st.append(cur_node)
 
-            elif s[i] == ")":
+        #     elif s[i] == ")":
 
-                if st:
-                    top = st.pop()
+        #         if st:
+        #             top = st.pop()
 
-                    if not st[-1].left:
-                        st[-1].left = top
-                    else:
-                        st[-1].right = top
+        #             if not st[-1].left:
+        #                 st[-1].left = top
+        #             else:
+        #                 st[-1].right = top
 
-            i += 1
+        #     i += 1
 
-        return st[-1]
+        # return st[-1]
+
+
+        # Solution 2 - Using recursion
+        # Time - 
+        # Space - 
+
+
+
+        def recursion(i):
+
+            if i >= len(s):
+                return None, i
+
+            neg = False
+            val = 0
+
+            if s[i] == "-":
+                neg = True
+                i += 1
+
+            while i < len(s) and s[i].isdigit():
+                val = val * 10 + int(s[i])
+                i += 1
+
+            if neg:
+                val *= -1
+
+            node = TreeNode(val)
+
+            if i < len(s) and s[i] == "(":
+                node.left, i = recursion(i + 1)
+                i += 1  # Skip the )
+
+
+            if i < len(s) and s[i] == "(":
+                node.right, i = recursion(i + 1)
+                i += 1  # Skip the )
+
+            return node, i
+
+        return recursion(0)[0]
