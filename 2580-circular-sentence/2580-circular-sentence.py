681c925c6de823aa136cfb892cbb2c843f1da3ec
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-11-02 14:06:38 -0700

    Time: 3 ms (12.23%), Space: 16.6 MB (46.28%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.py
+++ 2580-circular-sentence/2580-circular-sentence.py
@@ -3,18 +3,32 @@ class Solution:
         
 
         # Solution 1 - Splitting by space
+        # Time - O(n)
+        # Space - O(n)
+
+        # sentence = sentence.split(" ")
+
+        # if sentence[-1][-1] != sentence[0][0]:
+        #     return False
+
+        # for i in range(1, len(sentence)):
+
+        #     if sentence[i - 1][-1] != sentence[i][0]:
+        #         return False
+
+        # return True
+
+
+        # Solution 2 - Space optimised approach without splitting
         # Time - 
-        # Space - 
-
-        sentence = sentence.split(" ")
-
-        if sentence[-1][-1] != sentence[0][0]:
-            return False
-
-        for i in range(1, len(sentence)):
-
-            if sentence[i - 1][-1] != sentence[i][0]:
-                return False
+        # Space -  
 
 
-        return True  
+        for idx in range(len(sentence)):
+
+            if sentence[idx] == " ":
+                if sentence[idx - 1] != sentence[idx + 1]:
+                    return False
+
+                
+        return sentence[0] == sentence[-1]
