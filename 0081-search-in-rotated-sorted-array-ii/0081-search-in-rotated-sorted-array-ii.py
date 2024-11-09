9b8cec4d8f156b03972d949cc731417b1e0b73c5
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-11-09 12:32:29 -0800

    Time: 0 ms (100%), Space: 17.2 MB (39.15%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -35,26 +35,60 @@ class Solution:
         # return False
 
 
+        # l, r = 0, len(nums) - 1
+
+        # while l <= r:
+
+        #     mid = l + (r - l) // 2
+
+        #     if nums[mid] == target:
+        #         return True
+                
+        #     elif nums[l] < nums[mid]:
+        #         if nums[l] <= target < nums[mid]:
+        #             r = mid - 1
+        #         else:
+        #             l = mid + 1
+        #     elif nums[l] > nums[mid]:
+        #         if nums[mid] < target <= nums[r]:
+        #             l = mid + 1
+        #         else:
+        #             r = mid - 1
+        #     else:
+        #         l += 1
+            
+        # return False
+
+
+
         l, r = 0, len(nums) - 1
 
-        while l <= r:
+        while l <= r and nums[l] == nums[r]:
 
+            if nums[l] == target:
+                return True
+
+            l += 1
+            r -= 1
+
+        
+        while l <= r:
+            
             mid = l + (r - l) // 2
 
             if nums[mid] == target:
                 return True
-                
-            elif nums[l] < nums[mid]:
-                if nums[l] <= target < nums[mid]:
+
+            if nums[l] <= nums[mid]:
+                if nums[l] <= target <= nums[mid]:
                     r = mid - 1
                 else:
                     l = mid + 1
-            elif nums[l] > nums[mid]:
-                if nums[mid] < target <= nums[r]:
-                    l = mid + 1
-                else:
-                    r = mid - 1
+
             else:
-                l += 1
-            
-        return False
+                if nums[mid] <= target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+
+        return False 
