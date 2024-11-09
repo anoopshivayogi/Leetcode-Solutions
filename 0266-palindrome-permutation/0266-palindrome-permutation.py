2cc20628daa263858b19db42f797a3a2141be666
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-11-09 10:17:38 -0800

    Time: 0 ms (100%), Space: 16.5 MB (90.43%) - LeetHub

--- 0266-palindrome-permutation/0266-palindrome-permutation.py
+++ 0266-palindrome-permutation/0266-palindrome-permutation.py
@@ -3,18 +3,34 @@ class Solution:
 
 
         # Solution 1 - Using frequency and making sure only one odd frequency item is present
-        # Time - O(n)
+        # Time - O(2n)
         # Space - O(26) -- Since there are only 26 lowercase characters
         
-        seen_odd = False
-        freq = collections.Counter(s)
+        # seen_odd = False
+        # freq = collections.Counter(s)
 
-        for k, v in freq.items():
+        # for k, v in freq.items():
 
-            if v % 2 == 1:
-                if not seen_odd:
-                    seen_odd = True
-                else:
-                    return False
+        #     if v % 2 == 1:
+        #         if not seen_odd:
+        #             seen_odd = True
+        #         else:
+        #             return False
 
-        return True
+        # return True
+
+
+
+        # Solution 2 - Using set to count odd and evens smartly
+        # Time - O(n)
+        # Space - O(26)
+        
+        seen = set()
+
+        for c in s:
+            if c not in seen:
+                seen.add(c)
+            else:
+                seen.remove(c)
+
+        return len(seen) <= 1
