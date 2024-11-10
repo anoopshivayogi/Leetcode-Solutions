ac3952bedc9d355190bb841d1010ca49519f9b8f
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-11-09 20:11:01 -0800

    Time: 3 ms (98.14%), Space: 17.3 MB (53.96%) - LeetHub

--- 0529-minesweeper/0529-minesweeper.py
+++ 0529-minesweeper/0529-minesweeper.py
@@ -18,23 +18,62 @@ class Solution:
         # Time - O(m * n)
         # Space - O(m * n)
 
-        x, y = click
+        # x, y = click
 
-        if not (0 <= x < len(board) and 0 <= y < len(board[0])):
-            return None
+        # if not (0 <= x < len(board) and 0 <= y < len(board[0])):
+        #     return None
 
-        if board[x][y] == "M":
-            board[x][y] = 'X'
+        # if board[x][y] == "M":
+        #     board[x][y] = 'X'
+        #     return board
+        # elif board[x][y] == "E":
+        #     mines = self.get_adjacent_mines(board, x, y)
+
+        #     if mines:
+        #         board[x][y] = str(mines)
+        #     else:
+        #         board[x][y] = 'B'
+        #         for r in range(x - 1, x + 2):
+        #             for c in range(y - 1, y + 2):
+        #                 self.updateBoard(board, [r, c])
+
+        # return board
+
+
+        # Solution 2 - BFS using queue
+        # https://www.youtube.com/watch?v=lla6QlAF4HQ&t=184s
+        # Time - 
+        # Space - 
+
+        if not board:
             return board
-        elif board[x][y] == "E":
-            mines = self.get_adjacent_mines(board, x, y)
 
-            if mines:
-                board[x][y] = str(mines)
-            else:
-                board[x][y] = 'B'
+        ROWS, COLS = len(board), len(board[0])
+        q = collections.deque()
+        q.append(click)
+
+        while q:
+            x, y = q.popleft()
+
+            if board[x][y] == "M":
+                board[x][y] = "X"
+                return board
+            elif board[x][y] == "E":
+                mines = 0
+
                 for r in range(x - 1, x + 2):
                     for c in range(y - 1, y + 2):
-                        self.updateBoard(board, [r, c])
+                        if 0 <= r < ROWS and 0 <= c < COLS and board[r][c] == "M":
+                            mines += 1
+                
+                if mines:
+                    board[x][y] = str(mines)
+                else:
+                    board[x][y] = "B"
+
+                    for r in range(x - 1, x + 2):
+                        for c in range(y - 1, y + 2):
+                            if 0 <= r < ROWS and 0 <= c < COLS:
+                                q.append((r, c))
 
         return board
