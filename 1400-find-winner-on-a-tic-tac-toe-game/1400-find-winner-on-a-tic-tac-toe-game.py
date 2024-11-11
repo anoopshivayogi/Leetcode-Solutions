7d878ca4298b3f75fb91bc70cad53c0e8f2c6586
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-11-10 20:41:19 -0500

    Time: 0 ms (100%), Space: 16.7 MB (43.88%) - LeetHub

--- 1400-find-winner-on-a-tic-tac-toe-game/1400-find-winner-on-a-tic-tac-toe-game.py
+++ 1400-find-winner-on-a-tic-tac-toe-game/1400-find-winner-on-a-tic-tac-toe-game.py
@@ -3,8 +3,9 @@ class Solution:
         
 
         # Solution 1 - Using counter for each row, col and diagnoals
-        # Time - 
-        # Space - 
+        # let n be the size of the board and m be the number of moves
+        # Time - O(m)
+        # Space - O(2n)
 
         n = 3
 
