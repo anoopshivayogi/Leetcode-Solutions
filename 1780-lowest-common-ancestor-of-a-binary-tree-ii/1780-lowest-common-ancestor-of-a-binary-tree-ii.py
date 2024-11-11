dd04e909e2b785a7ef36b376ecf33e07038d4402
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-11-11 18:13:18 -0500

    Time: 150 ms (32.85%), Space: 21.3 MB (21.18%) - LeetHub

--- 1780-lowest-common-ancestor-of-a-binary-tree-ii/1780-lowest-common-ancestor-of-a-binary-tree-ii.py
+++ 1780-lowest-common-ancestor-of-a-binary-tree-ii/1780-lowest-common-ancestor-of-a-binary-tree-ii.py
@@ -8,9 +8,9 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
-        # Solution 1 - 
-        # Time - 
-        # Space - 
+        # Solution 1 - DFS solution ; modified version of the primary LCA question
+        # Time - O(n)
+        # Space - O(h) = O(n) in worst case for a skwed tree the height will be equal to the number of nodes
 
         nodes_found = False
 
@@ -42,6 +42,11 @@ class Solution:
 
             return left or right
 
-        ans = dfs(root)
+        ans = dfs(root)  # NOTE: Apparently this is very important; You cannot directly return dfs() if <condition>
 
         return ans if nodes_found else None
+
+
+        # In the code for finding the lowest common ancestor (LCA) of two nodes in a binary tree, there’s a subtle reason why directly returning dfs(root) if nodes_found else None does not work as intended.
+
+# The issue lies in the evaluation of nodes_found in return dfs(root) if nodes_found else None. When this line is executed, nodes_found may not yet be True, even if the DFS (depth-first search) function dfs has identified the LCA. This is because dfs only marks nodes_found = True during the recursive exploration when the LCA node is identified. However, nodes_found may still be False at the time dfs(root) finishes, depending on the traversal order and whether the function was able to set nodes_found early in the traversal.
