c7e3794c0314b2d1e827aa246a7a340cc18358ca
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-11-11 18:29:08 -0500

    Time: 141 ms (63.31%), Space: 21.4 MB (21.18%) - LeetHub

--- 1780-lowest-common-ancestor-of-a-binary-tree-ii/1780-lowest-common-ancestor-of-a-binary-tree-ii.py
+++ 1780-lowest-common-ancestor-of-a-binary-tree-ii/1780-lowest-common-ancestor-of-a-binary-tree-ii.py
@@ -12,10 +12,55 @@ class Solution:
         # Time - O(n)
         # Space - O(h) = O(n) in worst case for a skwed tree the height will be equal to the number of nodes
 
-        nodes_found = False
+        # nodes_found = False
+
+        # def dfs(node):
+        #     nonlocal nodes_found
+
+        #     if not node:
+        #         return None
+
+        #     left = dfs(node.left)
+        #     right = dfs(node.right)
+
+        #     conditions = 0
+
+        #     if node.val == p.val or node.val == q.val:
+        #         conditions += 1
+            
+        #     if left:
+        #         conditions += 1
+
+        #     if right:
+        #         conditions += 1
+
+        #     if conditions >= 2:
+        #         nodes_found = True
+
+        #     if (node.val == p.val or node.val == q.val) or (left and right):
+        #         return node
+
+        #     return left or right
+
+        # ans = dfs(root)  # NOTE: Apparently this is very important; You cannot directly return dfs() if <condition> mainly because the nodes_found will be evaluated before the dfs() returns anything.
+
+        # return ans if nodes_found else None
+
+
+        # In the code for finding the lowest common ancestor (LCA) of two nodes in a binary tree, there’s a subtle reason why directly returning dfs(root) if nodes_found else None does not work as intended.
+
+# The issue lies in the evaluation of nodes_found in return dfs(root) if nodes_found else None. When this line is executed, nodes_found may not yet be True, even if the DFS (depth-first search) function dfs has identified the LCA. This is because dfs only marks nodes_found = True during the recursive exploration when the LCA node is identified. However, nodes_found may still be False at the time dfs(root) finishes, depending on the traversal order and whether the function was able to set nodes_found early in the traversal.
+
+
+        # Solution 2 - More easier is to have a check for p_found and q_found seperately
+        # Time - 
+        # Space - 
+
+        p_found, q_found = False, False
 
         def dfs(node):
-            nonlocal nodes_found
+            nonlocal p_found
+            nonlocal q_found
 
             if not node:
                 return None
@@ -23,30 +68,19 @@ class Solution:
             left = dfs(node.left)
             right = dfs(node.right)
 
-            conditions = 0
+            if node.val in (p.val, q.val):
+                if node.val == p.val:
+                    p_found = True
+                
+                if node.val == q.val:
+                    q_found = True
 
-            if node.val == p.val or node.val == q.val:
-                conditions += 1
-            
-            if left:
-                conditions += 1
-
-            if right:
-                conditions += 1
-
-            if conditions >= 2:
-                nodes_found = True
-
-            if (node.val == p.val or node.val == q.val) or (left and right):
                 return node
 
-            return left or right
+            if left and right:
+                return node
+            else:
+                return left or right
 
-        ans = dfs(root)  # NOTE: Apparently this is very important; You cannot directly return dfs() if <condition> mainly because the nodes_found will be evaluated before the dfs() returns anything.
-
-        return ans if nodes_found else None
-
-
-        # In the code for finding the lowest common ancestor (LCA) of two nodes in a binary tree, there’s a subtle reason why directly returning dfs(root) if nodes_found else None does not work as intended.
-
-# The issue lies in the evaluation of nodes_found in return dfs(root) if nodes_found else None. When this line is executed, nodes_found may not yet be True, even if the DFS (depth-first search) function dfs has identified the LCA. This is because dfs only marks nodes_found = True during the recursive exploration when the LCA node is identified. However, nodes_found may still be False at the time dfs(root) finishes, depending on the traversal order and whether the function was able to set nodes_found early in the traversal.
+        ans = dfs(root)
+        return ans if p_found and q_found else None
