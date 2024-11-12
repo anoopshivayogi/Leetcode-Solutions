c6d3915ed438d01e9d5b9e5c33c5082022f28d41
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-11-12 13:11:11 -0500

    Time: 31 ms (8.78%), Space: 17.6 MB (83.29%) - LeetHub

--- 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.py
+++ 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.py
@@ -3,9 +3,9 @@ class Solution:
 
 
         # Solution 1 - Using sorting
-        # Time - 
-        # Space - 
-
+        # Intuition - We know the second highest; third highest etc after updating 
+        # Time - O(nlogn)
+        # Space - O(n) -- in timsort of pythong sort() implementation
 
         nums.sort()
         res = 0
@@ -13,14 +13,13 @@ class Solution:
         for i in range(len(nums) - 1, -1, -1):
             res += nums[i] - nums[0]
 
-
         return res
 
 
 
         # Solution 1 - Using the intuition that incrementing (n-1) elements is same as decrementing 1 element
-        # Time - 
-        # Space - 
+        # Time - O(n)
+        # Space - O(1)
         
         # Think of the last step, adding 1 to all but one element completes the question, instead you can also decrementing 1 from the element which is bigger than others. Assume we want to equal min and max first, we add 1 to min, keep max untouched. But at the meantime, the other n-2 elements are also increased by 1, min has never got "closer" to these n-2 elements. So every step, we are ONLY 1 closer to the final result.
 
