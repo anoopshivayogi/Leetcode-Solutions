5decf3cb32cb1435e5be221fd95d85e2e74de09a
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-11-14 00:55:27 -0500

    Time: 8952 ms (7.09%), Space: 16.8 MB (42.8%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        
+        from functools import cache
+
+        # @cache
+        def dfs(i, j):
+            if j >= len(p) and i < len(s):
+                return False
+            
+            if i >= len(s) and  j >= len(p):
+                return True
+
+            match = i < len(s) and (s[i] == p[j] or p[j] == ".")
+
+            if (j + 1) < len(p) and p[j + 1] == "*":
+                return (match and dfs(i + 1, j)) or dfs(i, j + 2)
+                    
+            if match:
+                return dfs(i + 1, j + 1)
+
+            return False
+
+        return dfs(0, 0)
