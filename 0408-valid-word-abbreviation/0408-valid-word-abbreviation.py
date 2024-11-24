988dd97d7d6aae6d7ac5311749b7652f46c4db4d
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-11-24 16:06:17 -0500

    Time: 0 ms (100%), Space: 16.6 MB (73.46%) - LeetHub

---
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
@@ -0,0 +1,67 @@
+class Solution:
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+
+        # Using two moving pointers
+        # Time - O(N)
+        # Space - O(1)
+
+        # word_ptr, abbr_ptr = 0, 0
+
+        # while abbr_ptr < len(abbr):
+
+        #     if abbr[abbr_ptr].isdigit():
+        #         steps = 0
+
+        #         if abbr[abbr_ptr] == "0":
+        #             return False
+
+        #         while abbr_ptr < len(abbr) and abbr[abbr_ptr].isdigit():
+        #             steps = steps * 10 + int(abbr[abbr_ptr])
+        #             abbr_ptr += 1
+
+        #         word_ptr += steps
+
+        #     else:
+        #         if word_ptr >= len(word):
+        #             return False
+
+        #         if word[word_ptr] != abbr[abbr_ptr]:
+        #             return False
+
+        #         word_ptr += 1
+        #         abbr_ptr += 1
+
+        # return word_ptr == len(word)
+
+
+        # Re-do for the interview
+        # Time - 
+        # Space - 
+
+        word_ptr, abbr_ptr = 0, 0
+
+        while abbr_ptr < len(abbr) and word_ptr < len(word):
+
+            if abbr[abbr_ptr].isdigit():
+                if abbr[abbr_ptr] == "0":
+                    return False
+
+                skip_val = 0
+                while abbr_ptr < len(abbr) and abbr[abbr_ptr].isdigit():
+                    skip_val = skip_val * 10 + int(abbr[abbr_ptr])
+                    abbr_ptr += 1
+
+                # abbr_ptr -= 1   # Bring back one extra move
+
+                word_ptr += skip_val
+
+            else:
+                if abbr[abbr_ptr] != word[word_ptr]:
+                    return False
+
+                abbr_ptr += 1
+                word_ptr += 1
+
+        return abbr_ptr == len(abbr) and word_ptr == len(word)
+
+            
