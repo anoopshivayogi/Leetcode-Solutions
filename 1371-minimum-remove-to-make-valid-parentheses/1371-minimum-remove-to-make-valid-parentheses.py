3c191254616afee742be78466e4ced94e5b554d0
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-11-23 23:00:30 -0500

    Time: 35 ms (75.73%), Space: 19.5 MB (14.94%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.py
@@ -43,31 +43,31 @@ class Solution:
 
         # Slightly modified version
 
-        # res = []
-        # count = 0
+        res = []
+        count = 0
 
-        # for c in s:
+        for c in s:
 
-        #     if c == "(":
-        #         count += 1
-        #         res.append(c)
-        #     elif c == ")":
-        #         if count > 0:
-        #             count -=1
-        #             res.append(c)
-        #     else:
-        #         res.append(c)
+            if c == "(":
+                count += 1
+                res.append(c)
+            elif c == ")":
+                if count > 0:
+                    count -=1
+                    res.append(c)
+            else:
+                res.append(c)
 
-        # filtered = []
+        filtered = []
 
-        # for c in res[::-1]:
-        #     if c == "(" and count > 0:
-        #         count -= 1
-        #         continue
-        #     else:
-        #         filtered.append(c)
+        for c in res[::-1]:
+            if c == "(" and count > 0:
+                count -= 1
+                continue
+            else:
+                filtered.append(c)
 
-        # return "".join(filtered[::-1])
+        return "".join(filtered[::-1])
 
         
 
@@ -96,24 +96,24 @@ class Solution:
         # Time - O(3N)
         # Space - O(N)
 
-        st = []
-        to_delete = set()
+        # st = []
+        # to_delete = set()
 
-        for idx, c in enumerate(s):  # O(N)
-            if c == "(":
-                st.append(idx)
-            elif c == ")":
-                if st:
-                    st.pop()
-                else:
-                    to_delete.add(idx)
+        # for idx, c in enumerate(s):  # O(N)
+        #     if c == "(":
+        #         st.append(idx)
+        #     elif c == ")":
+        #         if st:
+        #             st.pop()
+        #         else:
+        #             to_delete.add(idx)
 
-        res = []
+        # res = []
 
-        to_delete = to_delete.union(st)
+        # to_delete = to_delete.union(st)
 
-        for i, c in enumerate(s):  # O(N)
-            if i not in to_delete:
-                res.append(c)
+        # for i, c in enumerate(s):  # O(N)
+        #     if i not in to_delete:
+        #         res.append(c)
 
-        return "".join(res)  # O(N)
+        # return "".join(res)  # O(N)
