ccbd560b662aa99c7cb5ebbe139eccac62e14a67
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-12-01 03:51:14 -0500

    Time: 3 ms (10.47%), Space: 17.4 MB (5.98%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -0,0 +1,87 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+
+        # res = []
+        # if not root:
+        #     return res
+
+        # from collections import deque
+
+        # q = deque([root])
+
+        # while q:
+        #     for i in range(len(q)):
+        #         popped = q.popleft()
+        #         if popped.right:
+        #             q.append(popped.right)
+        #         if popped.left:
+        #             q.append(popped.left)
+        #         if i == 0:
+        #             res.append(popped.val)
+
+        # return res
+
+
+        # Re-do for the interview
+        # Time - O(n) - Will traverse through each node atleast once.
+        # Space - O(n/2) = O(n) - Complete binary tree where the leaf of the tree will have O(n/2) nodes.
+
+        # res = []
+
+        # if not root:
+        #     return []
+
+        # from collections import deque
+
+        # q = deque()
+        # q.append(root)
+
+        # while q:
+        #     for i in range(len(q)):
+        #         cur_node = q.popleft()
+
+        #         if i == 0:
+        #             res.append(cur_node.val)
+
+        #         if cur_node.right:
+        #             q.append(cur_node.right)
+
+        #         if cur_node.left:
+        #             q.append(cur_node.left)
+
+        # return res
+
+
+        # Re-do again for the interview
+        # Time - 
+        # Space -
+
+        if not root:
+            return []
+
+        q = collections.deque()
+        q.append(root)
+
+        res = []
+
+        while q:
+
+            for i in range(len(q)):
+                cur_node = q.popleft()
+
+                if i == 0:
+                    res.append(cur_node.val)
+
+                if cur_node.right:
+                    q.append(cur_node.right)
+
+                if cur_node.left:
+                    q.append(cur_node.left)
+
+        return res
