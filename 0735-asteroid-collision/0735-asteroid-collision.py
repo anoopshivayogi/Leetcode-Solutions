4934b1030569d60185bc6bc22ce40ab8bf8ad576
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-12-04 18:09:25 -0500

    Time: 2 ms (95.72%), Space: 18.3 MB (7.25%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -0,0 +1,36 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+
+        # Solution 1 - using stack you can look back as required
+        # Time - O(n)
+        # Space - O(n)
+
+        st = [] 
+
+        for a in asteroids:
+            st.append(a)
+
+            while len(st) >= 2 and st[-1] < 0 and st[-2] >= 0:
+                a1 = st.pop()
+                a2 = st.pop()
+
+                if abs(a1) == a2:
+                    continue
+                else:
+                    if abs(a1) > a2:
+                        st.append(a1)
+                    else:
+                        st.append(a2)
+        return st
+
+
+        # Re-do for the interview
+        # Time - 
+        # Space -
+
+        # st = []
+
+        # for num in asteroids:
+        #     st.append(num)
+
+        #     while len(st) >= 2 and (num[-1] <= 0  
