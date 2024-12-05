2af48bf532a9ee8e44ebddd17e1e16859d2ed6fb
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-12-04 20:01:50 -0500

    Time: 59 ms (20.37%), Space: 30.8 MB (28.32%) - LeetHub

---
+++ 1909-buildings-with-an-ocean-view/1909-buildings-with-an-ocean-view.py
@@ -0,0 +1,49 @@
+class Solution:
+    def findBuildings(self, heights: List[int]) -> List[int]:
+
+        # Solution 2 - using max_seen so far
+        # Time - O(n)
+        # Space - O(1)
+
+        # max_seen = float('-inf')
+        # res = collections.deque()
+
+        # for i in range(len(heights) - 1, -1, -1):
+        #     if heights[i] > max_seen:
+        #         res.appendleft(i)
+        #     max_seen = max(max_seen, heights[i])
+
+        # return res
+
+        # Re-do for the interview
+        # Time - O(n)
+        # Space - O(1)
+
+        # max_seen = float('-inf')
+        # res = collections.deque()
+
+        # for i in range(len(heights)-1, -1, -1):
+        #     if heights[i] > max_seen:
+        #         res.appendleft(i)
+            
+        #     max_seen = max(max_seen, heights[i])
+
+        # return res
+
+
+        # NOTE: Followup : you can only iterate from the left
+        # Solution 2 - Using monotonic stack
+        # Time - 
+        # Space -  
+
+        # st = [4, 3, 1]
+
+        st = []
+
+        for idx, num in enumerate(heights):
+            while st and heights[st[-1]] <= num:
+                st.pop()
+
+            st.append(idx)
+
+        return st
