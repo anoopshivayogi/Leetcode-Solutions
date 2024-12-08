b19c95ae91dad5747c86ca496e0adc12785ff84f
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-12-08 03:19:13 -0500

    Time: 67 ms (52.86%), Space: 17.9 MB (6.82%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
@@ -32,17 +32,40 @@ class Solution:
         # Time - O(n)
         # Space - O(1)
 
-        res = l = count = 0
+        # res = l = count = 0
         
+        # for r in range(len(nums)):
+        #     if nums[r] == 0:
+        #         count += 1
+
+        #     if l <= r and count > k: # NOTE: l <= r and not l < r ; example : nums = [0,0,0,0] and k = 0
+        #         if nums[l] == 0:
+        #             count -= 1
+        #         l += 1
+            
+        #     res = max(res, r - l + 1)
+
+        # return res
+
+
+        # Re-do for the interview 
+        # Time - 
+        # Space - 
+
+        res = 0
+        count = 0
+        l = 0
+
         for r in range(len(nums)):
             if nums[r] == 0:
                 count += 1
 
-            if l <= r and count > k: # NOTE: l <= r and not l < r ; example : nums = [0,0,0,0] and k = 0
+            while l <=r and count > k:
                 if nums[l] == 0:
                     count -= 1
                 l += 1
-            
-            res = max(res, r - l + 1)
+
+            res = max(res, r-l+1)
 
         return res
+            
