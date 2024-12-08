3b738cfaa8282c6f51a7c22bc6efbead2e46cc74
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-12-08 01:43:29 -0500

    Time: 0 ms (100%), Space: 17.1 MB (6.56%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.py
@@ -0,0 +1,62 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+
+        # Solution 1 - Using stack
+        # Time - O(n)
+        # Space - O(n)
+
+        # st = []
+
+        # for c in s:
+        #     if c == ")" and st and st[-1] == "(":
+        #         st.pop()
+        #     else:
+        #         st.append(c)
+
+        # return len(st)
+
+
+        # Solution 2 - Without using stack
+        # Time - O(n)
+        # Space - O(1)
+
+        # res = 0  # closing bracket mismatch count (balance < 0)
+        # balance = 0  # '(' positive number meaning opening bracket mismatch
+
+        # for c in s:
+        #     if c == ")":
+        #         balance -= 1
+        #         if balance < 0:
+        #             balance = 0
+        #             res += 1
+        #     else:
+        #         balance += 1
+
+
+        # return res + balance
+
+        # return balance
+
+
+        # Re-do for the interview  
+        # Time - 
+        # Space - 
+
+
+        if not s:
+            return 0
+
+        count = 0
+        res = 0
+
+        for c in s:
+            if c == "(":
+                count += 1
+            else:
+                count -= 1
+
+                if count < 0:
+                    res += 1
+                    count = 0
+        
+        return res + count
