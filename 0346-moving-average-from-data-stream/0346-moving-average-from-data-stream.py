e8ecdd3696e18678224e1db8c5a152cc047baf8d
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-12-08 05:02:25 -0500

    Time: 3 ms (82.55%), Space: 20.4 MB (23.03%) - LeetHub

--- 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
@@ -5,25 +5,43 @@ class MovingAverage:
         # Time - O(1)
         # Space - O(N); where N is the size of the moving queue
 
-        from collections import deque
+        # from collections import deque
 
-        self.n = size
-        self.last_n = deque()
+        # self.n = size
+        # self.last_n = deque()
+        # self.window_sum = 0
+
+        # Re-do for the interview
+
+        self.size = size
+        self.nums = collections.deque()
         self.window_sum = 0
 
         # Solution 2 - You can implement circular queue if you want for this solution
 
     def next(self, val: int) -> float:
-        self.last_n.append(val)
+        # self.last_n.append(val)
+        # self.window_sum += val
+
+        # # [1, 2, 4, 4, 5]
+        # #          r
+        # if len(self.last_n) > self.n:
+        #     removed = self.last_n.popleft()
+        #     self.window_sum -= removed
+
+        # return self.window_sum / len(self.last_n)
+
+        # Re-do for the interview
+
         self.window_sum += val
+        self.nums.append(val)
 
-        # [1, 2, 4, 4, 5]
-        #          r
-        if len(self.last_n) > self.n:
-            removed = self.last_n.popleft()
-            self.window_sum -= removed
+        if len(self.nums) > self.size:
+            popped = self.nums.popleft()
+            self.window_sum -= popped
 
-        return self.window_sum / len(self.last_n)
+        return self.window_sum / len(self.nums)
+
 
 
         # Circular queue
