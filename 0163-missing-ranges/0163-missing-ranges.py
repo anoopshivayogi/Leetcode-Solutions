23eab16f9d4422b9fc9f9d58d711c18a76e74fca
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-12-08 03:14:09 -0500

    Time: 0 ms (100%), Space: 17.2 MB (9.14%) - LeetHub

--- 0163-missing-ranges/0163-missing-ranges.py
+++ 0163-missing-ranges/0163-missing-ranges.py
@@ -1,23 +1,41 @@
 class Solution:
     def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[List[int]]:
         
-        # Solution 1 -
-        # Time -
-        # Space -
+        # Solution 1 - Array traversing and edge case handling
+        # Time - O(n)
+        # Space - O(1) or O(n)
+
+        # if not nums:
+        #     return [[lower, upper]]
+
+        # res = []
+
+        # if lower != nums[0]:
+        #     res.append([lower, nums[0] - 1])
+
+        # for i in range(len(nums) - 1):
+        #     if nums[i+1] - nums[i] > 1:
+        #         res.append([nums[i] + 1, nums[i+1] - 1])
+
+        # if upper != nums[-1]:
+        #     res.append([nums[-1] + 1, upper])
+        
+        # return res
+
 
         if not nums:
             return [[lower, upper]]
 
         res = []
 
-        if lower != nums[0]:
+        if nums[0] != lower:
             res.append([lower, nums[0] - 1])
 
-        for i in range(len(nums) - 1):
-            if nums[i+1] - nums[i] > 1:
-                res.append([nums[i] + 1, nums[i+1] - 1])
+        for i in range(1, len(nums)):
+            if abs(nums[i-1] - nums[i]) > 1:
+                res.append([nums[i-1] + 1, nums[i] - 1])
 
-        if upper != nums[-1]:
+        if nums[-1] != upper:
             res.append([nums[-1] + 1, upper])
-        
+
         return res
