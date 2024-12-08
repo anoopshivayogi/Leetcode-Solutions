9f936199c55fab58f81f79ebc48e882079f82188
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-12-08 12:44:05 -0500

    Time: 0 ms (100%), Space: 29.2 MB (49.5%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.py
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.py
@@ -4,14 +4,28 @@ class Solution:
 
         l, r = 0, len(arr) - 1
 
-
         while l <= r:
 
             mid = (l + r) // 2
 
-            if arr[mid] < arr[mid + 1]:
-                l = mid + 1
-            else:
+            if arr[mid] > arr[mid + 1]:
                 r = mid - 1
+            else:
+                l = mid + 1
 
         return l
+
+
+        # Tracing 
+        #. 0. 1. 2. 3
+        # [0, 2, 1, 0]
+        #     l         
+        #  m
+        #  r      
+
+
+        #  0  1.  2. 3
+        # [0, 10, 5, 2]
+        #     l
+        #. r
+        #  m
