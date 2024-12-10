d83dd9348505ae6a4b695108d43b9d5e27c5dca5
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2024-12-10 09:39:23 -0500

    Time: 42 ms (62.86%), Space: 21.8 MB (20.7%) - LeetHub

---
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.py
@@ -0,0 +1,48 @@
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+
+        # Solution 1 : 
+
+        # st = []
+
+        # for l in s:
+        #     if st and st[-1][0] == l:
+        #         st[-1][1] += 1
+        #     else:
+        #         st.append([l, 1])
+
+        #     if st[-1][1] == k:
+        #         st.pop()
+
+        # res = ''
+
+        # for i in st:
+        #     res += i[0] * i[1]
+
+        # return res
+
+
+        # Re-do the interview
+        # Time - 
+        # Space - 
+
+
+        st = []
+
+
+        for c in s:
+            if st and st[-1][0] == c:
+                st[-1][1] += 1
+            else:
+                st.append([c, 1])
+
+            if st[-1][1] == k:
+                st.pop()
+
+
+        res = ''
+
+        for c, count in st:
+            res += c * count
+
+        return res
