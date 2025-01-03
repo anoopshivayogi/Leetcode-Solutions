5fe94be9917fadf1f04c65da4e451bbe8d129fc3
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2025-01-03 14:33:54 -0500

    Time: 103 ms (13.26%), Space: 32.1 MB (67.48%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.py
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.py
@@ -6,16 +6,34 @@ class Solution:
         # Time - O(n)
         # Space - O(n)
 
-        temp = [0] * len(nums)
+        # temp = [0] * len(nums)
+        # res = 0
+
+        # for idx in range(len(nums) - 2, -1, -1):
+        #     temp[idx] = (nums[idx + 1] + temp[idx + 1])
+
+        # pre_sum = 0
+        # for idx in range(len(nums) - 1):
+        #     pre_sum += nums[idx]
+        #     if pre_sum >= temp[idx]:
+        #         res += 1
+
+        # return res
+
+
+        # Solution 2 - Optimized prefix and suffix sums
+        # Time - 
+        # Space - 
+
+        left_sum, right_sum = 0, sum(nums)
         res = 0
 
-        for idx in range(len(nums) - 2, -1, -1):
-            temp[idx] = (nums[idx + 1] + temp[idx + 1])
-
-        pre_sum = 0
         for idx in range(len(nums) - 1):
-            pre_sum += nums[idx]
-            if pre_sum >= temp[idx]:
+            left_sum += nums[idx]
+            right_sum -= nums[idx]
+
+            if left_sum >= right_sum:
                 res += 1
+
 
         return res
