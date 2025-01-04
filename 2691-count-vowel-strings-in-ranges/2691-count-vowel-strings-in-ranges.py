a9b747aae40ab70e262e6694a3b34a9b46e2f4bd
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2025-01-03 19:55:55 -0500

    Time: 80 ms (5.01%), Space: 49.5 MB (97.03%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.py
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.py
@@ -3,7 +3,7 @@ class Solution:
         
 
         # Solution 1 - Using prefix + suffix sum
-        # Time - O(m + n)
+        # Time - O(2m + n)
         # Space - O(m)
         
         #  idx     0. 1. 2. 3. 4.
@@ -14,36 +14,67 @@ class Solution:
         #         total = 4
 
 
-        left = [0] * len(words)
-        prefix = 0
+        # left = [0] * len(words)
+        # prefix = 0
 
+        # vowels = {'a', 'e', 'i', 'o', 'u'}
+
+        # for idx in range(len(words)):
+        #     left[idx] += prefix
+
+        #     if words[idx] and (words[idx][0] in vowels) and (words[idx][-1] in vowels):
+        #         prefix += 1
+
+        # total = prefix
+
+        # right = [0] * len(words)
+        # postfix = 0
+
+        # for idx in range(len(words) - 1, -1, -1):
+        #     right[idx] += postfix
+
+        #     if words[idx] and (words[idx][0] in vowels) and (words[idx][-1] in vowels):
+        #         postfix += 1
+
+        # res = [0] * len(queries)
+
+        # for idx, q in enumerate(queries):
+        #     cur_res = total
+
+        #     cur_res -= left[q[0]]
+        #     cur_res -= right[q[1]]
+
+        #     res[idx] = cur_res
+
+        # return res
+
+        # Solution 2 - Just using prefix - more optimised with single pass and O(m) instead of O(2m)
+        # Time - O(m + n)
+        # Space - O(m)
+
+        #  0  1. 2. 3. 4
+        # [1, 1, 2, 3, 4]
+
+
+        prefix = [0] * len(words)
         vowels = {'a', 'e', 'i', 'o', 'u'}
+        cur_prefix = 0
 
         for idx in range(len(words)):
-            left[idx] += prefix
-
             if words[idx] and (words[idx][0] in vowels) and (words[idx][-1] in vowels):
-                prefix += 1
+                cur_prefix += 1
 
-        total = prefix
+            prefix[idx] = cur_prefix
 
-        right = [0] * len(words)
-        postfix = 0
-
-        for idx in range(len(words) - 1, -1, -1):
-            right[idx] += postfix
-
-            if words[idx] and (words[idx][0] in vowels) and (words[idx][-1] in vowels):
-                postfix += 1
+        print(prefix)
 
         res = [0] * len(queries)
-
         for idx, q in enumerate(queries):
-            cur_res = total
-
-            cur_res -= left[q[0]]
-            cur_res -= right[q[1]]
-
-            res[idx] = cur_res
+            if q[0] == 0:
+                print("res")
+                res[idx] = prefix[q[1]]
+            else:
+                print(idx, prefix[q[1]], prefix[q[0]])
+                res[idx] = prefix[q[1]] - prefix[q[0] - 1]
 
         return res
