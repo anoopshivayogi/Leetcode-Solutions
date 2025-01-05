be09910139f5bfdf4456e88b518f7dedad183855
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2025-01-05 11:30:27 -0500

    Time: 3002 ms (5.04%), Space: 18.3 MB (29.98%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.py
@@ -2,18 +2,18 @@ class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
         
         # Solution 1 - Using two pointers + Hashset(to avoid duplicates)
-        # Time - 
-        # Space - 
+        # Time - O(n*26)
+        # Space - O(26)
 
         left, right = set(), Counter(s)
-        res = set()
+        res = set() # Space - O(26^2) - Two spots to fill in the three char palindrome : _ _ _
 
-        for c in s:
+        for c in s:  # O(n)
             right[c] -= 1
             if right[c] == 0:
                 del right[c]
 
-            for j in range(26):
+            for j in range(26):  # O(26)
                 cur_char = chr(ord('a') + j)
                 if (cur_char in left) and (cur_char in right):
                     res.add(cur_char + c + cur_char)
