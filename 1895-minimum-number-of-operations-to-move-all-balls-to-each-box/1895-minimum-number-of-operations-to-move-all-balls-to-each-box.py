ba03323e5400c8f7913ae3267ddb4c6b2a34274d
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2025-01-07 11:59:11 -0500

    Time: 7 ms (89.26%), Space: 18.3 MB (6.2%) - LeetHub

---
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -0,0 +1,39 @@
+class Solution:
+    def minOperations(self, boxes: str) -> List[int]:
+        
+        
+        # [0,  0, 1, 0, 1, 1]
+        # [11, 8, 5, 4, 3, 4]
+
+
+        # [0,  0, 1, 0, 1, 1]
+        # [11, 8, 5, 4, 3, 4]
+
+        # Solution 1 - Prefix and suffix sum
+        # Time - 
+        # Space - 
+
+        res = [0] * len(boxes)
+        prefix_sum = 0
+        inc = 0
+
+        for idx in range(len(boxes)):
+            res[idx] += prefix_sum
+
+            if boxes[idx] == "1":
+                inc += 1
+
+            prefix_sum += inc
+
+        postfix_sum = 0
+        inc = 0
+
+        for idx in range(len(boxes) - 1, -1, -1):
+            res[idx] += postfix_sum
+
+            if boxes[idx] == "1":
+                inc += 1
+
+            postfix_sum += inc
+
+        return res
