b7c2c747bfdb3c53931a1e3545434903cdad3a71
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2025-01-23 19:26:29 -0500

    Time: 16 ms (67.03%), Space: 19.7 MB (22.58%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.py
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.py
@@ -6,29 +6,52 @@ class Solution:
         # Space - O(m * n)
 
 
-        if not grid:
-            return 0
+        # if not grid:
+        #     return 0
+
+        # ROWS, COLS = len(grid), len(grid[0])
+        # row_seen, col_seen = [set() for _ in range(ROWS)], [set() for _ in range(COLS)]
+
+        # for r in range(ROWS):
+        #     for c in range(COLS):
+
+        #         if grid[r][c]:
+        #             row_seen[r].add((r, c))
+        #             col_seen[c].add((r, c))
+
+        # res = set()
+
+        # for r in range(ROWS):
+        #     if len(row_seen[r]) > 1:
+        #         for ele in row_seen[r]:
+        #             res.add(ele)
+
+        # for c in range(COLS):
+        #     if len(col_seen[c]) > 1:
+        #         for ele in col_seen[c]:
+        #             res.add(ele)
+
+        # return len(res)
+
+
+        # Solution 2 - Using just rows and cols counter
+        # Time - O(m * n)
+        # Space - O(m + n)
 
         ROWS, COLS = len(grid), len(grid[0])
-        row_seen, col_seen = [set() for _ in range(ROWS)], [set() for _ in range(COLS)]
+        row_count, col_count = [0] * ROWS, [0] * COLS
 
         for r in range(ROWS):
             for c in range(COLS):
-
                 if grid[r][c]:
-                    row_seen[r].add((r, c))
-                    col_seen[c].add((r, c))
-
-        res = set()
-
+                    row_count[r] += 1
+                    col_count[c] += 1
+        
+        res = 0
         for r in range(ROWS):
-            if len(row_seen[r]) > 1:
-                for ele in row_seen[r]:
-                    res.add(ele)
+            for c in range(COLS):
+                if grid[r][c]:
+                    if row_count[r] > 1 or col_count[c] > 1:
+                        res += 1
 
-        for c in range(COLS):
-            if len(col_seen[c]) > 1:
-                for ele in col_seen[c]:
-                    res.add(ele)
-
-        return len(res)
+        return res
