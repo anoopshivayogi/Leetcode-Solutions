bc1df680b65c69e607096f854ce80ab2c82d68e0
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2025-03-17 18:21:12 -0700

    Time: 6 ms (22.83%), Space: 17.9 MB (72.71%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.py
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.py
@@ -4,13 +4,43 @@ class Solution:
         # Time - O(n)
         # Space - O(n)
 
-        freq = Counter(nums)
+        # freq = Counter(nums)
 
-        for k, v in freq.items():
-            if v % 2:
-                return False
+        # for k, v in freq.items():
+        #     if v % 2:
+        #         return False
 
-        return True
+        # return True
 
 
-        # Solution 1 - 
+        # Solution 1 - Understand why XOR won't work in this case
+        # Time - O(n)
+        # Space - O(1)
+
+        # Counter Example : 
+        # Given the array [1, 2, 4, 7], computing the XOR:
+        # 1⊕2⊕4⊕7=0
+        # A result of 0 means that each bit position had an even number of 1s. But this doesn't tell us if elements appeared exactly twice.
+
+        # val = nums[0]
+
+        # for ele in nums[1:]:
+        #     val ^= ele
+
+        # print(val)
+
+        # return True if (not val) else False
+
+        # Solution 2 - Using sets
+        # Time - 
+        # Space - 
+
+        unpaired = set()
+
+        for n in nums:
+            if n in unpaired:
+                unpaired.remove(n)
+            else:
+                unpaired.add(n)
+
+        return not unpaired
