403a45ef5f68301fa54513f28da84915ad0f232a
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2025-04-22 09:38:33 -0700

    Time: 0 ms (100%), Space: 18.1 MB (8.84%) - LeetHub

---
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        
+        # Solution 1 - using hasmap
+        # Time - 
+        # Space -
+
+        freq = defaultdict(int)
+        res = 0
+
+        for ele in answers:
+            if ele == 0:
+                res += 1
+                continue
+            
+            freq[ele] += 1
+            if freq[ele] == ele + 1:
+                res += freq[ele]
+                del freq[ele]
+
+        for k in freq:
+            res += (k + 1)
+
+        return res
+
+
+
