152698f5e7f4af134e7b2a52e81b0ec5bfc637eb
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2025-07-14 11:42:51 -0400

    Time: 0 ms (100%), Space: 17.6 MB (74.88%) - LeetHub

--- 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.py
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.py
@@ -6,30 +6,43 @@
 class Solution:
     def getDecimalValue(self, head: Optional[ListNode]) -> int:
         
-        # Solution 1 - Reverse the linkedlist and using 2^(nth node)
-        # Time - O(2n)
+        # # Solution 1 - Reverse the linkedlist and using 2^(nth node)
+        # # Time - O(2n)
+        # # Space - O(1)
+
+        # # Reverse a linkedlist
+        # prev = None
+        # cur = head
+
+        # while cur:
+        #     temp = cur.next
+        #     cur.next = prev
+        #     prev = cur
+        #     cur = temp
+
+        # cur = prev
+
+        # # Traverse and use 2^count
+        # count = 0
+        # res = 0
+
+        # while cur:
+        #     if cur.val:
+        #         res += (2 ** count)
+        #     cur = cur.next
+        #     count += 1
+
+        # return res
+
+
+        # Solution 2 - Using the logic of how a binary number is built if you read from MSB to LSB (arithmetic)
+        # Time - O(n)
         # Space - O(1)
 
-        # Reverse a linkedlist
-        prev = None
-        cur = head
+        num = head.val
 
-        while cur:
-            temp = cur.next
-            cur.next = prev
-            prev = cur
-            cur = temp
+        while head.next:
+            num = num * 2 + head.next.val
+            head = head.next
 
-        cur = prev
-
-        # Traverse and use 2^count
-        count = 0
-        res = 0
-
-        while cur:
-            if cur.val:
-                res += (2 ** count)
-            cur = cur.next
-            count += 1
-
-        return res
+        return num
