c184eb5fbe6a977a5b07d7ba3e2adc74312abe90
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2025-07-15 13:16:52 -0400

    Time: 3 ms (18.53%), Space: 17.6 MB (86.06%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isValid(self, word: str) -> bool:
+        
+
+        # Solution 1 - Straightforward
+        # Time - 
+        # Space - 
+
+        if len(word) < 3:
+            return False
+
+        vowels = {'a', 'e', 'i', 'o', 'u'}
+        vowel_flag = False
+        consonant_flag = False
+
+        for c in word:
+            if c.isalnum():
+                if c.lower() in vowels:
+                    vowel_flag = True
+                elif not c.isdigit():
+                    consonant_flag = True
+            else:
+                return False
+        
+        if vowel_flag and consonant_flag:
+            return True
+        else:
+            print(vowel_flag, consonant_flag)
+            return False
+
+
