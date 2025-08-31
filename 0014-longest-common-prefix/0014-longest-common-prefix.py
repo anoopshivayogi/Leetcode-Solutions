38994844e0ac45ad66bf09803740ba40fcaefc47
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2025-08-31 12:22:49 -0400

    Time: 3 ms (25.71%), Space: 18.1 MB (8.12%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -38,7 +38,7 @@ class Solution:
         idx = 0
 
         for i in range(len(strs[0])):
-            for s in strs[1:]:
+            for s in strs:
                 if i == len(s) or strs[0][i] != s[i]:
                     return res
             res += strs[0][i]
