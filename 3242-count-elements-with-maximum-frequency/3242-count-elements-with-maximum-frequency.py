86bfefe6cb9949054fde804a6af851aa38c16a31
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2025-09-22 19:32:04 -0400

    Time: 3 ms (34.95%), Space: 17.9 MB (34.38%) - LeetHub

---
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.py
@@ -0,0 +1,45 @@
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        # Solution 1 - Bucket sort   
+        # Time - O(n)
+        # Space - O(n)
+
+        from collections import Counter
+
+        n = len(nums)
+        freq_lst = [0] * (n+1)
+
+        freq = Counter(nums) 
+
+        for k, v in freq.items():
+            freq_lst[v] += 1
+
+        print(freq_lst)
+
+        for idx in range(n, -1, -1):
+            if freq_lst[idx] > 0:
+                return freq_lst[idx]*idx
+
+
+        # from collections import Counter, defaultdict
+
+        # n = len(nums)
+        # freq_lst = [0] * (n+1)
+        # max_freq = float('inf')
+
+        # freq = defaultdict(int)
+
+        # for n in nums:
+        #     freq[n] += 1
+        #     max_freq = max(freq[n], max)
+
+        # for k, v in freq.items():
+        #     freq_lst[v] += 1
+
+        # print(freq_lst)
+
+        # for idx in range(n, -1, -1):
+        #     if freq_lst[idx] > 0:
+        #         return freq_lst[idx]*idx
+
+        
