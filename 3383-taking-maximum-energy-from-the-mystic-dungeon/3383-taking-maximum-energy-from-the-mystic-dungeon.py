44d0aedb71bf81aec49b5999ac87e12fa151c598
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2025-10-10 14:08:29 -0400

    Time: 1109 ms (79.38%), Space: 31.1 MB (79.38%) - LeetHub

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.py
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.py
@@ -3,15 +3,10 @@ class Solution:
         
 
 
-        # [5, 2, -10, -5, 1] k = 3 ; ans = 100
+        # arr = [5, 2, -10, -5, 1] k = 3 ; ans = 100
 
 
-        # arr[i] = max(arr[i-k] + arr[i], arr[i])
-        # arr[0] = max(arr[-3] + arr[i], arr[0])
-
-        # arr = [5, 2, -10, 0, 3]
-
-        # max(arr)
+        # dp = [5, 3, -10, -5, 1]
 
 
         # Solution 1 - Using dynamic programming 
@@ -19,23 +14,21 @@ class Solution:
         # Space - 
 
 
-        # n = len(energy)
-        # dp = [0] * n
-
-        # for i in range(n):
-        #     if i - k > 0:
-        #         dp[i] = max(dp[i - k] + energy[i], dp[i])
-        #     else:
-        #         dp[i] = energy[i]
-
-        # return max(dp)
-
         n = len(energy)
         dp = [0] * n
-        result = float('-inf')
+
         for i in range(n - 1, -1, -1):
             dp[i] = energy[i] + (dp[i + k] if i + k < n else 0)
-            result = max(result, dp[i])
 
         return max(dp)
 
+
+
+        # n = len(energy)
+        # dp = [0] * n
+        # result = float('-inf')
+        # for i in range(n - 1, -1, -1):
+        #     dp[i] = energy[i] + (dp[i + k] if i + k < n else 0)
+        #     result = max(result, dp[i])
+
+        # return max(dp)
