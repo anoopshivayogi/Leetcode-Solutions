131786ba5b8f1bdc005f0051fb68c1435330acaf
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2025-11-09 11:35:02 -0500

    Time: 0 ms (100%), Space: 17.9 MB (40.13%) - LeetHub

--- 1086-divisor-game/1086-divisor-game.py
+++ 1086-divisor-game/1086-divisor-game.py
@@ -1,3 +1,20 @@
 class Solution:
     def divisorGame(self, n: int) -> bool:
+
+        # Solution 1 - Most optimized; A game between even and odd
+        # Time - O(1)
+        # Space - O(1)
+
+        # Intuition - 
+        # Understand that for someone to lose the game; they have to be ending up with n == 1 (odd)
+        # Since Alice has the first change and she starts the game with even; she can always make it odd for bob by chosing 1
+        # If alice always ends up with even and bob odd then bob will end up with 1 and lose the game.
+        # The reverse is also true; if alice ends up with odd; then bob can make it odd for alice every time by chosing 1.
+
         return n % 2 == 0
+
+
+        # Solution 2 - Dynamic programming
+        # Time - 
+        # Space - 
+
