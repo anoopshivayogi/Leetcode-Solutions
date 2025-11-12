b8ff04b6cbfad0d265807704737a7230a6a6c4ad
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2025-11-11 20:04:01 -0500

    Time: 0 ms (100%), Space: 18.3 MB (41.34%) - LeetHub

---
+++ 1076-brace-expansion/1076-brace-expansion.py
@@ -0,0 +1,38 @@
+class Solution:
+    def expand(self, s: str) -> List[str]:
+        
+
+        # Solution 1 - backtracking/dfs
+        # Time - 
+        # Space -
+
+        inp = []
+        idx = 0
+        while idx < len(s):
+            cur = s[idx]
+            if ord(cur) >= ord('a') and ord(cur) <= ord('z'):
+                inp.append(cur)
+            elif cur == '{':
+                inner = []
+                while cur != '}':
+                    idx += 1
+                    cur = s[idx]
+                    if cur not in {'}', ','}:
+                        inner.append(cur)
+                inner.sort()
+                inp.append(inner)
+            idx += 1
+
+        res = []
+
+        def dfs(idx, cur):
+            if idx == len(inp):
+                res.append(cur)
+                return
+
+            for ch in inp[idx]:
+                dfs(idx + 1, cur + ch)
+
+        dfs(0, '')
+
+        return res
