51fa45b25df66e10a53f5fa95c6985edf98dbbab
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2025-11-16 12:46:17 -0500

    Time: 96 ms (72.12%), Space: 28.5 MB (23.87%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -44,29 +44,14 @@ class Solution:
         # Time - O(n)
         # Space - O(1)
 
-        # l, r = 0, len(height) - 1
-        # res = 0
-
-        # while l < r:
-        #     res = max(res, min(height[l], height[r]) * (r - l))
-        #     if height[l] <= height[r]:
-        #         l += 1
-        #     else:
-        #         r -= 1
-
-        # return res
-
-
         l, r = 0, len(height) - 1
         res = 0
 
         while l < r:
             res = max(res, min(height[l], height[r]) * (r - l))
-            if height[l] < height[r]:
+            if height[l] <= height[r]:
                 l += 1
             else:
                 r -= 1
-        
-        
 
         return res
