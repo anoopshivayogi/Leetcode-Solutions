fe21a5a63b03ccbfd40099622dd0ea52024c171b
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2026-04-14 19:15:36 -0500

    Time: 0 ms (100%), Space: 19.4 MB (20.97%) - LeetHub

---
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def closestTarget(self, words: List[str], target: str, startIndex: int) -> int:
+        
+
+        # Solution 1 - Using two pointers
+        # Time - 
+        # Space - 
+
+
+        delta = 0
+        n = len(words)
+
+        while delta != n:
+            if words[(startIndex + delta) % n] == target:
+                return delta
+            elif words[(startIndex - delta + n) % n] == target:
+                return delta
+
+            delta += 1
+
+        return -1
