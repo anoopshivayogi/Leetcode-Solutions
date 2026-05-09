4ea166cf65b9246cbafb90e733ee61d49825f119
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2026-05-09 18:44:47 -0500

    Time: 51 ms (49.95%), Space: 90.9 MB (27.46%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,17 @@
+#include <unordered_set>
+using namespace std;
+
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+
+        for(int num: nums){
+            if(!seen.insert(num).second){
+                return true;
+            }
+        }
+        return false;
+    }
+};
