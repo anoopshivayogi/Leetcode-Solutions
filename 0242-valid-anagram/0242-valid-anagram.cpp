d49a20577f3116e2b31df0ca2b850ad648326e8f
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2026-05-09 19:49:54 -0500

    Time: 2 ms (49.73%), Space: 10 MB (10.54%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <unordered_map>
 
 class Solution {
 public:
@@ -16,28 +17,42 @@ public:
         // return s == t;
 
 
-        // Solution 2 - using frequency count
+        // Solution 2 - using unordered_dict
 
         if(s.size() != t.size()){
             return false;
         }
+
+        unordered_map<char, int> countS;
+        unordered_map<char, int> countT;
+
+        for(char c: s) countS[c]++;
+        for(char c: t) countT[c]++;
+
+        return countS == countT;
+
+
+        // Solution 3 - using frequency count
+
+        // if(s.size() != t.size()){
+        //     return false;
+        // }
         
-        int count[26] = {0};
+        // int count[26] = {0};
 
-        for(int i=0; i<s.size(); i++){
-            count[s[i] - 'a']++;
-        }
+        // for(int i=0; i<s.size(); i++){
+        //     count[s[i] - 'a']++;
+        // }
 
-        for(int i=0; i<t.size();i++){
-            count[t[i] - 'a']--;
-        }
+        // for(int i=0; i<t.size();i++){
+        //     count[t[i] - 'a']--;
+        // }
 
-        for(int i=0; i<26; i++){
-            if(count[i] != 0){
-                return false;
-            }
-        }
-
-        return true;
+        // for(int i=0; i<26; i++){
+        //     if(count[i] != 0){
+        //         return false;
+        //     }
+        // }
+        // return true;
     }
 };
