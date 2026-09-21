5f1097cb4612e93085ddc1b0ed0b45a6968b9a8e
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2026-09-20 19:13:56 -0500

    Time: 7 ms (69.58%), Space: 19.3 MB (55.53%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -3,10 +3,8 @@ class Solution:
         
 
         res = 0
-        pos = 1
 
-        for ch in s:
-            res += pos * (26 - (ord(ch) - ord('a')))
-            pos += 1
+        for idx, ch in enumerate(s, start=1):
+            res += idx * (26 - (ord(ch) - ord('a')))
 
         return res
